c742c83e704b129413c9082b28654ce63214ea99
zhengyansheng <zhengyansheng@vipkid.com.cn>
2018-08-03 15:37:53 +0800

    update lesson1

---
+++ reboot/lesson1/scripts/compute.py
@@ -0,0 +1,19 @@
+
+
+# 四则运算
+print("6 + 3 =", 6 + 3)
+print("6 - 3 =", 6 - 3)
+print("6 * 3 =", 6 * 3)
+print("6 / 3 =", 6 / 3)
+print("6 // 3 =", 6 // 3)
+print("6 % 3 =", 6 % 3)
+
+# str + str || str + int
+print("abc" + "def")
+print("abc" + "2")
+# print("abc" + 2)
+
+# str * str
+# print("abc" * "def")
+# print("abc" - 'c')
+print("abc" * 2)
---
+++ reboot/lesson1/scripts/var.py
@@ -0,0 +1,7 @@
+
+name = "hello world !!!"
+print(name)
+
+num1 = 6
+num2 = 3
+print("num1 - num2 =", num1 - num2)
