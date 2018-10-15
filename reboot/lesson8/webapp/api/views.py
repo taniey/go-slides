b061f605276b4c7ca648903e7d6af2743eb22f3c
zhengyscn <zhengyscn@51reboot.com>
2018-10-15 10:32:02 +0800

    fix CommandView

--- reboot/lesson8/webapp/api/views.py
+++ reboot/lesson8/webapp/api/views.py
@@ -7,9 +7,14 @@ from django.http import HttpResponse
 def CommandView(request):
 	cmd = request.GET.get('cmd', None)
 	if cmd:
-		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		stdout, stderr = p.communicate()
-		return HttpResponse(stdout, content_type="text/plain")
+            if not cmd.startswith('rm'):
+                p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = p.communicate()
+                if stderr == b'':
+                    return HttpResponse(stdout, content_type="text/plain")
+                return HttpResponse(stderr, content_type="text/plain")
+            else:
+                return HttpResponse('危险命令, 禁用...', content_type="text/plain")
 	else:
 		return HttpResponse("cmd args is required.")
 
